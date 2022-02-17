bf2d546872f1f3cb4f8f741d2ac6fd3ad043c22e
Guilherme Nunes <94137931+Breaik@users.noreply.github.com>
2022-02-17 13:41:45 -0300

    commit

---
+++ aula05/Controlador.java
@@ -0,0 +1,14 @@
+package com.aula05;
+
+public interface Controlador {
+    public void liga();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pause();
+}
---
+++ aula05/main.java
@@ -0,0 +1,10 @@
+package com.aula05;
+
+public class main {
+    public static void main(String[] args) {
+        ControleRemoto c1 = new ControleRemoto();
+
+        c1.abrirMenu();
+
+    }
+}
